7c5ae63169b98a1fb3536ce466b8b564f7153c12
Jade Allen Cook <jadeallencook@gmail.com>
2017-09-01 11:33:35 -0600

    ⛏ Installed auth NPM pages and built pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ArtistComponent } from './pages/artist/artist.component';
+import { BookComponent } from './pages/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    ArtistComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
