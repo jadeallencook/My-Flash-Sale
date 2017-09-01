057c649b3fb1e00c41b1789936a47b7bda654ef0
Jade Allen Cook <jadeallencook@gmail.com>
2017-09-01 13:44:04 -0600

    Completed homepage and finished routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ArtistComponent } from './pages/artist/artist.component';
 import { BookComponent } from './pages/book/book.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'artist/:id', component: ArtistComponent },
+  { path: 'book', component: BookComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +25,17 @@ import { BookComponent } from './pages/book/book.component';
     HomeComponent,
     DashboardComponent,
     ArtistComponent,
-    BookComponent
+    BookComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
